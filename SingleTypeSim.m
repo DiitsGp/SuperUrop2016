199d53c2b41017d11079afb24a02e6b884bac6d1
Aditya Gopalan <aditya@gopalans.net>
2016-11-28 15:38:26 -0500

    Simulate standard potential function for Marden example

---
+++ SingleTypeSim.m
@@ -0,0 +1,60 @@
+%% SingleTypeSim.m
+% For this code I will assume the following:
+% There are m routes from a source S to a destination D, with flows denoted
+%    by f_1 ... f_m
+% There are n edges from the same source S to the same destination D,
+%   with flows denoted by g_1 ... g_n and cost functions c_1(g_1) ...
+%   c_n(g_n)
+% Let F be the vector [f_1 ... f_m]'
+% Let G be the vector [g_1 ... g_n]'
+% Let C be the vector [c_1(g_1) ... c_n(g_n)]'
+% So that F, G, C are vector-functions of time
+% Let A be the adjency matrix of size nxm so that:
+%   A_ij = 1 iff edge i belongs to route j and A_ij = 0 otherwise
+% We will simulate the dynamics of the system from t=1 to t=T for the
+%   example given in Marden et al.'s 'Joint Strategy Fictitious Play With
+%   Inertia for Potential Games'
+
+%% Initialize
+rng(0);
+
+% In the Marden example there are 10 edges, each corresponding to a unique
+%   route. Each route contains exactly one edge.
+A = eye(10);
+[m, n] = size(A);
+
+% Generate a random initial condition with one unit of flow in total
+F0 = rand(m, 1);
+F0 = F0/sum(F0);
+F = F0;
+F_tilde = zeros(m, 1);
+
+% Generate the coefficients for quadratic cost functions as in the Marden
+%   et al. paper
+C_coeff = rand(n, 3); %a_i x^2 + b_i x + c_i
+C = zeros(n, 1);
+
+% Simulate for T = 10^3 timesteps
+T = 10^3;
+eta = 1/sqrt(T);
+F_evol = zeros(m, T); % for plotting
+
+%% Simulate
+for i = 1:T
+   F_evol(:, i) = F;
+   G = A*F;
+   for j = 1:n
+      c = C_coeff(j, :);
+      g = G(j);
+      C(n) = c(1)*g^2 + c(2)*g + c(3);
+   end
+   grad_phi = A'*C;
+   F_tilde = F - eta*grad_phi;
+   
+   F = projsplx(F_tilde);
+end
+
+figure
+plot(1:1:T, F_evol);
+title('Traffic Flow to Equilibrium of the Standard Potential Function on Marden Example');
+legend('route 1', 'route 2', 'route 3', 'route 4', 'route 5', 'route 6', 'route 7', 'route 8', 'route 9', 'route 10');
